52a0ee65630b522a21f979dbf0559f7f5de9185b
Sophie Peneva <39370712+sophiepeneva@users.noreply.github.com>
2019-01-15 10:06:24 +0200

    Create FileRead.java

---
+++ FileRead.java
@@ -0,0 +1,31 @@
+# SAP_Initial_Taskts
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+
+public class FileRead {
+    public static String readFile(String filename) {
+        String content = null;
+        File file = new File(filename);
+        FileReader reader = null;
+        try {
+            reader = new FileReader(file);
+            char[] chars = new char[(int) file.length()];
+            reader.read(chars);
+            content = new String(chars);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return content;
+    }
+}
